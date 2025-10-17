dba1d64af5fef6bb2e2d2538535b33ef984a9e16
andezzzWW <andezzzww@gmail.com>
2025-10-17 15:53:53 +0300

    Update InstallEGO.ps1

--- InstallEGO.ps1
+++ InstallEGO.ps1
@@ -1,2 +1,3 @@
-Set-Content -Path "приветствие.txt" -Value "привет !" -Encoding UTF8
-Write-Host "Файл создан!"
+$filePath = Join-Path $env:LOCALAPPDATA "привет.txt"
+"привет" | Out-File -FilePath $filePath -Encoding UTF8
+Invoke-Item $filePath
